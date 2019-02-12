83bd41195153ac17144837ab9681485705e2609e
Brad McKinney <brad.mckin@gmail.com>
2019-02-12 11:22:36 -1000

    update users seeds

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -5,9 +5,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { first_name: 'testUser1', last_name: 'name1', email: 'test1@example.com' },
-        { first_name: 'testUser2', last_name: 'name2', email: 'test2@example.com' },
-        { first_name: 'testUser3', last_name: 'name3', email: 'test3@example.com' }
+        { username: 'user1', password: 'abc123', first_name: 'testUser1', last_name: 'name1', email: 'test1@example.com' },
+        { username: 'user2', password: 'abc123', first_name: 'testUser2', last_name: 'name2', email: 'test2@example.com' },
+        { username: 'user3', password: 'abc123', first_name: 'testUser3', last_name: 'name3', email: 'test3@example.com' }
       ]);
     });
 };
