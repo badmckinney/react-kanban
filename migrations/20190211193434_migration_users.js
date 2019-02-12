2793e491db780a06bb58b312a542450d63133e64
Brad McKinney <brad.mckin@gmail.com>
2019-02-12 11:12:56 -1000

    test routes

--- migrations/20190211193434_migration_users.js
+++ migrations/20190211193434_migration_users.js
@@ -2,6 +2,8 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('users', function (table) {
     table.increments();
+    table.string('username').notNullable();
+    table.string('password').notNullable();
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
     table.string('email').notNullable();
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/', (req, res) => {
+  res.json({ success: true });
+});
+
+router.put('/', (req, res) => {
+  res.json({ success: true });
+});
+
+router.delete('/', (req, res) => {
+  res.json({ success: true });
+});
+
+module.exports = router;
