e388a41f3ab7f2a8f3755c89935722580a83df86
Brad McKinney <brad.mckin@gmail.com>
2019-02-14 14:32:22 -1000

    add seeds for cards table

--- seeds/04_cards.js
+++ seeds/04_cards.js
@@ -5,9 +5,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('cards').insert([
-        // {id: 1, colName: 'rowValue1'},
-        // {id: 2, colName: 'rowValue2'},
-        // {id: 3, colName: 'rowValue3'}
+        { title: 'do dishes', body: 'make sure they sparkle', priority_id: 1, status_id: 1, created_by: 1, assigned_to: 2 },
+        { title: 'eat food', body: 'eat alot', priority_id: 2, status_id: 2, created_by: 2, assigned_to: 3 },
+        { title: 'fix changes', body: 'let us know if you have questions', priority_id: 3, status_id: 3, created_by: 3, assigned_to: 1 }
       ]);
     });
 };
