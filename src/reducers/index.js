0ba0533bd5f19d8affc46d9f1fe53af93b6683e0
Brad McKinney <brad.mckin@gmail.com>
2019-02-15 12:53:28 -1000

    create reducer functions for ADD_CARD and DELETE_CARD

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
-import { LOAD_CARDS } from '../actions';
+import { LOAD_CARDS, ADD_CARD, DELETE_CARD } from '../actions';
 
 const cardReducer = (state = [], action) => {
   switch (action.type) {
     case LOAD_CARDS:
-      return action.payload;
+      return [].concat(action.payload);
+    case ADD_CARD:
+      return [...state, action.payload];
+    case DELETE_CARD:
+      return [].concat(action.payload);
     default:
       return state;
   }
