b87475128beede15e3d7f0cce96678211658a959
Brad McKinney <brad.mckin@gmail.com>
2019-02-14 14:34:10 -1000

    remove unused code and comment out auth code for now

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const session = require('express-session');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const bcrypt = require('bcryptjs');
+// const passport = require('passport');
+// const bcrypt = require('bcryptjs');
 const redis = require('connect-redis')(session);
-const LocalStrategy = require('passport-local');
+// const LocalStrategy = require('passport-local');
 const flash = require('connect-flash');
-const router = require('./routes/main');
+const router = require('./routes/cards');
 
 const ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 8080;
@@ -27,7 +27,6 @@ if (!SESSION_SECRET) {
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({
   store: new redis({ url: 'redis://redis-server:6379', logErrors: true }),
@@ -36,63 +35,62 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  return done(null, {
-    id: user.id,
-    username: user.username,
-    role_id: user.role_id
-  });
-});
+// passport.serializeUser((user, done) => {
+//   return done(null, {
+//     id: user.id,
+//     username: user.username,
+//     role_id: user.role_id
+//   });
+// });
 
-passport.deserializeUser((user, done) => {
-  new User({ id: user.id }).fetch()
-    .then(dbUser => {
-      if (dbUser === null) {
-        return done();
-      }
-      dbUser = dbUser.toJSON();
-      return done(null, {
-        id: dbUser.id,
-        username: dbUser.username,
-        role_id: dbUser.role_id
-      });
-    })
-    .catch((err) => {
-      return done(err);
-    });
-});
+// passport.deserializeUser((user, done) => {
+//   new User({ id: user.id }).fetch()
+//     .then(dbUser => {
+//       if (dbUser === null) {
+//         return done();
+//       }
+//       dbUser = dbUser.toJSON();
+//       return done(null, {
+//         id: dbUser.id,
+//         username: dbUser.username,
+//         role_id: dbUser.role_id
+//       });
+//     })
+//     .catch((err) => {
+//       return done(err);
+//     });
+// });
 
-passport.use(new LocalStrategy(function (username, password, done) {
-  return new User({ username: username }).fetch()
-    .then(dbUser => {
-      if (dbUser === null) {
-        return done(null, false, { message: 'That username does not exist' });
-      }
-      else {
-        dbUser = dbUser.toJSON();
+// passport.use(new LocalStrategy(function (username, password, done) {
+//   return new User({ username: username }).fetch()
+//     .then(dbUser => {
+//       if (dbUser === null) {
+//         return done(null, false, { message: 'That username does not exist' });
+//       }
+//       else {
+//         dbUser = dbUser.toJSON();
 
-        bcrypt.compare(password, dbUser.password)
-          .then((res) => {
-            if (res) {
-              return done(null, dbUser);
-            } else {
-              return done(null, false, { message: 'bad username or password' });
-            }
-          });
-      }
-    })
-    .catch(err => {
-      return done(err);
-    });
-}));
+//         bcrypt.compare(password, dbUser.password)
+//           .then((res) => {
+//             if (res) {
+//               return done(null, dbUser);
+//             } else {
+//               return done(null, false, { message: 'bad username or password' });
+//             }
+//           });
+//       }
+//     })
+//     .catch(err => {
+//       return done(err);
+//     });
+// }));
 
-app.use('/', router);
+app.use('/cards', router);
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
