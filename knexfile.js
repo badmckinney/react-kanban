a90ecf229265394c6d728196301dd7c5be3f1064
Brad McKinney <brad.mckin@gmail.com>
2019-02-14 14:55:17 -1000

    load in dotenv and require into knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,11 @@
+const dotenv = require('dotenv');
+
 module.exports = {
   client: 'pg',
   connection: {
     host: process.env.PG_HOST,
     user: process.env.PG_USER,
-    password: process.env.PG_PW,
+    password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE
   },
   pool: {
