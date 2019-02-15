189eb4aa58ac1c722d3e4742c998cef4e9bf4168
Brad McKinney <brad.mckin@gmail.com>
2019-02-14 14:37:33 -1000

    add load cards action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,22 @@
 /**
  * Actions
  */
-
+export const LOAD_CARDS = 'LOAD_CARDS';
 
 /**
  * Action Creators
  */
+export const loadCards = () => {
+  return (dispatch) => {
+    return fetch('/cards')
+      .then((response) => {
+        return response.json();
+      })
+      .then((cards) => {
+        return dispatch({
+          type: LOAD_CARDS,
+          payload: cards
+        });
+      });
+  }
+}
