47fdee9aaf39dc3989ecf507737597d2bc26f500
Brad McKinney <brad.mckin@gmail.com>
2019-02-14 14:35:30 -1000

    remove hard-coded data and create load cards switch into reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,36 +1,9 @@
+import { LOAD_CARDS } from '../actions';
 
-const cards = [
-  {
-    id: 1,
-    title: 'Do dishes',
-    body: 'Make sure they sparkle',
-    priority: 'Medium',
-    assignedBy: 'Tiana',
-    assignedTo: 'Brad',
-    status: 'In Queue'
-  },
-  {
-    id: 2,
-    title: 'Eat Food',
-    body: 'Eat a lot',
-    priority: 'High',
-    assignedBy: 'Brad',
-    assignedTo: 'Brad',
-    status: 'In Progress'
-  },
-  {
-    id: 3,
-    title: 'Fix changes',
-    body: 'Let us know if you have any questions',
-    priority: 'Low',
-    assignedBy: 'Jason',
-    assignedTo: 'Brad',
-    status: 'Done'
-  },
-];
-
-const cardReducer = (state = cards, action) => {
+const cardReducer = (state = [], action) => {
   switch (action.type) {
+    case LOAD_CARDS:
+      return action.payload;
     default:
       return state;
   }
