edf32cae6b5b16483ac59a09ece1c956279698d0
Brad McKinney <brad.mckin@gmail.com>
2019-02-15 12:47:50 -1000

    create routes for POST and DELETE

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const Cards = require('../../database/models/Card');
+const Card = require('../../database/models/Card');
 const Priority = require('../../database/models/Priority');
 const Status = require('../../database/models/Status');
 const User = require('../../database/models/User');
 
+/************************
+ *  GET
+************************/
+
 router.get('/', (req, res) => {
-  Cards.fetchAll({
+  Card.fetchAll({
     withRelated: ['priority', 'status', 'createdBy', 'assignedTo']
   })
     .then((cards) => {
@@ -37,16 +41,94 @@ router.get('/', (req, res) => {
     })
 });
 
+/************************
+ * POST
+************************/
+
 router.post('/', (req, res) => {
-  res.json({ success: true });
+  const newCard = req.body;
+
+  Card.forge({
+    title: newCard.title,
+    body: newCard.body,
+    priority_id: newCard.priority_id,
+    status_id: newCard.status_id,
+    created_by: newCard.created_by,
+    assigned_to: newCard.assigned_to
+  }).save(null, { method: 'insert' })
+    .then(() => {
+      new Card({ title: newCard.title }).fetch({
+        withRelated: ['priority', 'status', 'createdBy', 'assignedTo']
+      })
+        .then((card) => {
+          let relations = card.relations;
+          const priority = relations.priority.attributes.name;
+          const status = relations.status.attributes.name;
+          const assignedBy = relations.createdBy.attributes.first_name;
+          const assignedTo = relations.assignedTo.attributes.first_name;
+
+          const cardData = {
+            id: card.id,
+            title: card.attributes.title,
+            body: card.attributes.body,
+            priority: priority,
+            status: status,
+            assignedBy: assignedBy,
+            assignedTo: assignedTo
+          }
+
+          return res.json(cardData);
+        });
+    });
 });
+
+/************************
+ *  PUT
+************************/
 
 router.put('/', (req, res) => {
   res.json({ success: true });
 });
 
+/************************
+ *  DELETE
+************************/
+
 router.delete('/', (req, res) => {
-  res.json({ success: true });
+  const id = req.body.id;
+  new Card({ id: id })
+    .destroy()
+    .then(() => {
+      Card.fetchAll({
+        withRelated: ['priority', 'status', 'createdBy', 'assignedTo']
+      })
+        .then((cards) => {
+          cardList = cards.models;
+          cards = [];
+
+
+          cardList.forEach(card => {
+            let relations = card.relations;
+            const priority = relations.priority.attributes.name;
+            const status = relations.status.attributes.name;
+            const assignedBy = relations.createdBy.attributes.first_name;
+            const assignedTo = relations.assignedTo.attributes.first_name;
+
+            const cardData = {
+              id: card.id,
+              title: card.attributes.title,
+              body: card.attributes.body,
+              priority: priority,
+              status: status,
+              assignedBy: assignedBy,
+              assignedTo: assignedTo
+            }
+
+            cards.push(cardData);
+          });
+          res.json(cards);
+        });
+    });
 });
 
 module.exports = router;
