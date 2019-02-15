cc35e604eab842afcdae1a3588aba82666e4c456
Brad McKinney <brad.mckin@gmail.com>
2019-02-15 12:56:41 -1000

    upgrade Card component to smart component to manage delete and edit button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deleteCard } from '../../actions';
 import './Card.scss';
 
-const Card = (props) => {
+class Card extends Component {
 
-  return (
-    <div className="card">
-      <div className="card-top">
-        <h3 className="task-name">{props.title}</h3>
-        <p className="id">Card-id #{Array(Math.max(3 - String(props.id).length + 1, 0)).join(0) + props.id}</p>
-      </div>
-      <p className="body">{props.body}</p>
-      <p className="priority">Priority: {props.priority}</p>
-      <p className="assigned-by">Assigned by: {props.assignedBy}</p>
-      <div className="card-bottom">
-        <div className="card-buttons">
-          <button className="edit">Edit</button>
-          <button className="delete">Delete</button>
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete(e) {
+    const id = parseInt(e.target.id);
+    this.props.onDelete({ id });
+  }
+
+
+  render() {
+    return (
+      <div className="card">
+        <div className="card-top">
+          <h3 className="task-name">{this.props.title}</h3>
+          <p className="id">Task-id #{Array(Math.max(3 - String(this.props.id).length + 1, 0)).join(0) + this.props.id}</p>
         </div>
-        <p className="assigned-to">{props.status} – {props.assignedTo}</p>
+        <p className="body">{this.props.body}</p>
+        <p className="priority">Priority: {this.props.priority}</p>
+        <p className="assigned-by">Assigned by: {this.props.assignedBy}</p>
+        <div className="card-bottom">
+          <div className="card-buttons">
+            <button className="edit">Edit</button>
+            <button id={this.props.id} className="delete" onClick={this.handleDelete}>Delete</button>
+          </div>
+          <p className="assigned-to">{this.props.status} – {this.props.assignedTo}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
+
+const mapStateToProps = (state) => {
+  return {};
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onDelete: (id) => {
+      dispatch(deleteCard(id));
+    }
+  }
+};
+
+Card = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Card);
 
 export default Card;
