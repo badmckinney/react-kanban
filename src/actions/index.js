c03cd83e5cf21cbf98fdf6c2e1ab84ea8717fc25
Brad McKinney <brad.mckin@gmail.com>
2019-02-15 12:54:27 -1000

    create addCard and deleteCard actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@
  * Actions
  */
 export const LOAD_CARDS = 'LOAD_CARDS';
+export const ADD_CARD = 'ADD_CARD';
+export const DELETE_CARD = 'DELETE_CARD';
 
 /**
  * Action Creators
@@ -20,3 +22,46 @@ export const loadCards = () => {
       });
   }
 }
+
+export const addCard = (card) => {
+  return (dispatch) => {
+    return fetch('/cards', {
+      method: 'POST',
+      body: JSON.stringify(card),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((body) => {
+        return dispatch({
+          type: ADD_CARD,
+          payload: body
+        });
+      });
+  }
+};
+
+export const deleteCard = (id) => {
+  console.log('action', id);
+  return (dispatch) => {
+    return fetch('/cards', {
+      method: 'DELETE',
+      body: JSON.stringify(id),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((cards) => {
+        return dispatch({
+          type: DELETE_CARD,
+          payload: cards
+        });
+      });
+  }
+};
