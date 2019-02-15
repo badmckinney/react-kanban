c1d4dcb01a42e112ec0d177c38ed586ecb36b048
Brad McKinney <brad.mckin@gmail.com>
2019-02-15 12:59:00 -1000

    relocate AddCardButton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Header.scss';
+import AddCardButton from '../../containers/AddCardButton';
 
 const Header = (props) => {
-  const { header, handleAddClick } = props;
+  const { header } = props;
 
   return (
     <div className="header">
       <h1 className="header-title">{header}</h1>
-      <button className="add" onClick={handleAddClick}>+ NEW TASK</button>
+      <AddCardButton />
     </div>
   )
 };
