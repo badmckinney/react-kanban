d44dbfe2a5876cf223fcf8a9839b0a734d94eb5f
Brad McKinney <brad.mckin@gmail.com>
2019-02-16 11:45:59 -1000

    add lifecycle functions

--- src/containers/EditCard/EditCard.jsx
+++ src/containers/EditCard/EditCard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { editCard } from '../../actions';
+import UserList from '../UserList';
 import './EditCard.scss';
 
 class EditCard extends Component {
@@ -8,6 +9,7 @@ class EditCard extends Component {
     super(props);
 
     this.state = {
+      id: '',
       title: '',
       body: '',
       priority_id: '',
@@ -23,6 +25,38 @@ class EditCard extends Component {
     this.handleAssignedByOnChange = this.handleAssignedByOnChange.bind(this);
     this.handleAssignedToOnChange = this.handleAssignedToOnChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState(this.props.getCardData());
+  }
+
+  componentDidMount() {
+    const statusRadio = document.querySelectorAll('input[name=status_id]');
+    statusRadio.forEach(radio => {
+      if (radio.value === this.state.status_id) {
+        radio.checked = true;
+      }
+    });
+
+    const priorityRadio = document.querySelectorAll('input[name=priority_id]');
+    priorityRadio.forEach(radio => {
+      if (radio.value === this.state.priority_id) {
+        radio.checked = true;
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.setState({
+      id: '',
+      title: '',
+      body: '',
+      priority_id: '',
+      status_id: '',
+      created_by: '',
+      assigned_to: ''
+    });
   }
 
   handleTitleOnChange(e) {
@@ -58,9 +92,10 @@ class EditCard extends Component {
   handleSubmit(e) {
     e.preventDefault();
     this.props.close();
-    const { title, body, priority_id, status_id, created_by, assigned_to } = this.state;
+    const { id, title, body, priority_id, status_id, created_by, assigned_to } = this.state;
 
     this.props.onEdit({
+      id,
       title,
       body,
       priority_id,
@@ -70,6 +105,7 @@ class EditCard extends Component {
     });
 
     this.setState({
+      id: '',
       title: '',
       body: '',
       priority_id: '',
@@ -129,11 +165,15 @@ class EditCard extends Component {
         </div>
         <div className="created-by">
           <label htmlFor="created_by">Assigned By:</label>
-          <input type="text" name="created_by" value={this.state.created_by} onChange={this.handleAssignedByOnChange} />
+          <select name="created_by" defaultValue={this.state.created_by} onChange={this.handleAssignedByOnChange}>
+            <UserList selected={this.state.created_by} />
+          </select>
         </div>
         <div className="assigned-to">
           <label htmlFor="assigned_to">Assigned To:</label>
-          <input type="text" name="assigned_to" value={this.state.assigned_to} onChange={this.handleAssignedToOnChange} />
+          <select name="assigned_to" defaultValue={this.state.assigned_to} onChange={this.handleAssignedToOnChange}>
+            <UserList selected={this.state.assigned_to} />
+          </select>
         </div>
         <button onClick={this.handleSubmit}>Submit</button>
       </form>
