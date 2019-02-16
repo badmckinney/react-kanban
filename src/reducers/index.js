8ff743cc1e4427c3fe2f1ec59d58e7c3a871f6b3
Brad McKinney <brad.mckin@gmail.com>
2019-02-16 11:45:24 -1000

    add loadusers and edit card reducer functions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,18 @@
-import { LOAD_CARDS, ADD_CARD, DELETE_CARD } from '../actions';
+import { LOAD_CARDS, LOAD_USERS, ADD_CARD, EDIT_CARD, DELETE_CARD } from '../actions';
 
 const cardReducer = (state = {
-  cards: []
+  cards: [],
+  users: []
 }, action) => {
   switch (action.type) {
     case LOAD_CARDS:
       return Object.assign({}, state, { cards: action.payload });
+    case LOAD_USERS:
+      return Object.assign({}, state, { users: action.payload });
     case ADD_CARD:
       return Object.assign({}, state, { cards: [...state.cards, action.payload] });
+    case EDIT_CARD:
+      return Object.assign({}, state, { cards: action.payload });
     case DELETE_CARD:
       return Object.assign({}, state, { cards: action.payload });
     default:
