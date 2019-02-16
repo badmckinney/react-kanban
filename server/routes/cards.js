a985b65d86a49368b26f334dfce253de9e094009
Brad McKinney <brad.mckin@gmail.com>
2019-02-16 11:41:58 -1000

    update routes to pull more data from database and create get users route

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -17,27 +17,50 @@ router.get('/', (req, res) => {
       cardList = cards.models;
       cards = [];
 
-
       cardList.forEach(card => {
         let relations = card.relations;
-        const priority = relations.priority.attributes.name;
-        const status = relations.status.attributes.name;
-        const assignedBy = relations.createdBy.attributes.first_name;
-        const assignedTo = relations.assignedTo.attributes.first_name;
+        const priority = relations.priority.attributes;
+        const status = relations.status.attributes;
+        const assignedBy = relations.createdBy.attributes;
+        const assignedTo = relations.assignedTo.attributes;
 
         const cardData = {
           id: card.id,
           title: card.attributes.title,
           body: card.attributes.body,
-          priority: priority,
-          status: status,
-          assignedBy: assignedBy,
-          assignedTo: assignedTo
+          priority: priority.name,
+          priority_id: priority.id,
+          status: status.name,
+          status_id: status.id,
+          assignedBy: assignedBy.first_name,
+          assignedBy_id: assignedBy.id,
+          assignedTo: assignedTo.first_name,
+          assignedTo_id: assignedTo.id
         }
 
         cards.push(cardData);
       });
       res.json(cards);
+    })
+});
+
+router.get('/users', (req, res) => {
+  User.fetchAll({
+    columns: ['id', 'first_name']
+  })
+    .then((users) => {
+      userList = users.models;
+      users = [];
+
+      userList.forEach(user => {
+        const userData = {
+          id: user.attributes.id,
+          first_name: user.attributes.first_name
+        }
+
+        users.push(userData);
+      });
+      res.json(users);
     })
 });
 
@@ -71,10 +94,14 @@ router.post('/', (req, res) => {
             id: card.id,
             title: card.attributes.title,
             body: card.attributes.body,
-            priority: priority,
-            status: status,
-            assignedBy: assignedBy,
-            assignedTo: assignedTo
+            priority: priority.name,
+            priority_id: priority.id,
+            status: status.name,
+            status_id: status.id,
+            assignedBy: assignedBy.first_name,
+            assignedBy_id: assignedBy.id,
+            assignedTo: assignedTo.first_name,
+            assignedTo_id: assignedTo.id
           }
 
           return res.json(cardData);
@@ -89,37 +116,48 @@ router.post('/', (req, res) => {
 router.put('/', (req, res) => {
   const card = req.body;
 
-  new Card({ id: card.id })
+  Card.where({ id: card.id })
     .save({
       title: card.title,
       body: card.body,
-      priority: card.priority,
-      status: card.status,
+      priority_id: card.priority_id,
+      status_id: card.status_id,
       created_by: card.created_by,
       assigned_to: card.assigned_to
     }, { patch: true })
     .then(() => {
-      new Card({ id: card.id }).fetch({
+      Card.fetchAll({
         withRelated: ['priority', 'status', 'createdBy', 'assignedTo']
       })
-        .then((card) => {
-          let relations = card.relations;
-          const priority = relations.priority.attributes.name;
-          const status = relations.status.attributes.name;
-          const assignedBy = relations.createdBy.attributes.first_name;
-          const assignedTo = relations.assignedTo.attributes.first_name;
+        .then((cards) => {
+          cardList = cards.models;
+          cards = [];
 
-          const cardData = {
-            id: card.id,
-            title: card.attributes.title,
-            body: card.attributes.body,
-            priority: priority,
-            status: status,
-            assignedBy: assignedBy,
-            assignedTo: assignedTo
-          }
 
-          return res.json(cardData);
+          cardList.forEach(card => {
+            let relations = card.relations;
+            const priority = relations.priority.attributes;
+            const status = relations.status.attributes;
+            const assignedBy = relations.createdBy.attributes;
+            const assignedTo = relations.assignedTo.attributes;
+
+            const cardData = {
+              id: card.id,
+              title: card.attributes.title,
+              body: card.attributes.body,
+              priority: priority.name,
+              priority_id: priority.id,
+              status: status.name,
+              status_id: status.id,
+              assignedBy: assignedBy.first_name,
+              assignedBy_id: assignedBy.id,
+              assignedTo: assignedTo.first_name,
+              assignedTo_id: assignedTo.id
+            }
+
+            cards.push(cardData);
+          });
+          res.json(cards);
         });
     });
 });
