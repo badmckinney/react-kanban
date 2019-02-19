e7e3542f751520f0850bf6c01577c82094d5d982
Brad McKinney <brad.mckin@gmail.com>
2019-02-19 11:45:17 -1000

    create logic to protect edit and delete buttons from unauthenticated users

--- src/containers/Card/Card.jsx
+++ src/containers/Card/Card.jsx
@@ -21,6 +21,30 @@ class Card extends Component {
 
 
   render() {
+
+    if (this.props.auth) {
+      return (
+        <div className="card">
+          <div className="card-top">
+            <h3 className="task-name">{this.props.title}</h3>
+            <p className="id">Task-id #{Array(Math.max(3 - String(this.props.id).length + 1, 0)).join(0) + this.props.id}</p>
+          </div>
+          <p className="body">{this.props.body}</p>
+          <p className="priority">Priority: {this.props.priority}</p>
+          <p className="assigned-by">Assigned by: {this.props.assignedBy}</p>
+          <div className="card-bottom">
+            <div className="card-buttons" data-id={this.props.id} data-title={this.props.title} data-body={this.props.body}
+              data-priority={this.props.priority_id} data-assignedby={this.props.assignedBy_id} data-status={this.props.status_id}
+              data-assignedto={this.props.assignedTo_id}>
+              <EditCardButton />
+              <button id={this.props.id} className="delete" onClick={this.handleDelete}>Delete</button>
+            </div>
+            <p className="assigned-to">{this.props.status} – {this.props.assignedTo}</p>
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div className="card">
         <div className="card-top">
@@ -34,8 +58,6 @@ class Card extends Component {
           <div className="card-buttons" data-id={this.props.id} data-title={this.props.title} data-body={this.props.body}
             data-priority={this.props.priority_id} data-assignedby={this.props.assignedBy_id} data-status={this.props.status_id}
             data-assignedto={this.props.assignedTo_id}>
-            <EditCardButton />
-            <button id={this.props.id} className="delete" onClick={this.handleDelete}>Delete</button>
           </div>
           <p className="assigned-to">{this.props.status} – {this.props.assignedTo}</p>
         </div>
@@ -45,7 +67,7 @@ class Card extends Component {
 };
 
 const mapStateToProps = (state) => {
-  return {};
+  return { auth: state.auth };
 }
 
 const mapDispatchToProps = (dispatch) => {
