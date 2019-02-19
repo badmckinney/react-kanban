d6fbdd744e57a888c2059b84e3fa2fbf2e974278
Brad McKinney <brad.mckin@gmail.com>
2019-02-19 11:28:33 -1000

    bring in passport authentication logic

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const session = require('express-session');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
-// const passport = require('passport');
-// const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
 const redis = require('connect-redis')(session);
-// const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local');
 const flash = require('connect-flash');
 const router = require('./routes/cards');
+const authRouter = require('./routes/auth');
+const User = require('../database/models/User');
 
 const ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 8080;
@@ -37,60 +39,59 @@ app.use(session({
 
 app.use(methodOverride('_method'));
 app.use(flash());
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// passport.serializeUser((user, done) => {
-//   return done(null, {
-//     id: user.id,
-//     username: user.username,
-//     role_id: user.role_id
-//   });
-// });
+passport.serializeUser((user, done) => {
+  return done(null, {
+    id: user.id,
+    username: user.username,
+  });
+});
 
-// passport.deserializeUser((user, done) => {
-//   new User({ id: user.id }).fetch()
-//     .then(dbUser => {
-//       if (dbUser === null) {
-//         return done();
-//       }
-//       dbUser = dbUser.toJSON();
-//       return done(null, {
-//         id: dbUser.id,
-//         username: dbUser.username,
-//         role_id: dbUser.role_id
-//       });
-//     })
-//     .catch((err) => {
-//       return done(err);
-//     });
-// });
+passport.deserializeUser((user, done) => {
+  new User({ id: user.id }).fetch()
+    .then(dbUser => {
+      if (dbUser === null) {
+        return done();
+      }
+      dbUser = dbUser.toJSON();
+      return done(null, {
+        id: dbUser.id,
+        username: dbUser.username,
+      });
+    })
+    .catch((err) => {
+      return done(err);
+    });
+});
 
-// passport.use(new LocalStrategy(function (username, password, done) {
-//   return new User({ username: username }).fetch()
-//     .then(dbUser => {
-//       if (dbUser === null) {
-//         return done(null, false, { message: 'That username does not exist' });
-//       }
-//       else {
-//         dbUser = dbUser.toJSON();
-
-//         bcrypt.compare(password, dbUser.password)
-//           .then((res) => {
-//             if (res) {
-//               return done(null, dbUser);
-//             } else {
-//               return done(null, false, { message: 'bad username or password' });
-//             }
-//           });
-//       }
-//     })
-//     .catch(err => {
-//       return done(err);
-//     });
-// }));
+passport.use(new LocalStrategy(function (username, password, done) {
+  return new User({ username: username }).fetch()
+    .then(dbUser => {
+      if (dbUser === null) {
+        return done(null, false, { message: 'That username does not exist' });
+      }
+      else {
+        dbUser = dbUser.toJSON();
+        bcrypt.compare(password, dbUser.password)
+          .then((res) => {
+            if (res) {
+              return done(null, dbUser);
+            } else {
+              return done(null, false, { message: 'bad username or password' });
+            }
+          });
+      }
+    })
+    .catch(err => {
+      console.log('catch')
+      return done(err);
+    });
+}));
 
 app.use('/cards', router);
+app.use('/auth', authRouter);
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
