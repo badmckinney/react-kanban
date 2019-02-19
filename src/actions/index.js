1b08c9997a546dd94d2fa0b531bacb7b4252579b
Brad McKinney <brad.mckin@gmail.com>
2019-02-19 11:29:38 -1000

    create ADD_USER, LOGIN_USER, and LOGOUT_USER actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,11 @@
 export const LOAD_CARDS = 'LOAD_CARDS';
 export const LOAD_USERS = 'LOAD_USERS';
 export const ADD_CARD = 'ADD_CARD';
+export const ADD_USER = 'ADD_USER';
 export const DELETE_CARD = 'DELETE_CARD';
 export const EDIT_CARD = 'EDIT_CARD';
+export const LOGIN_USER = 'LOGIN_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
 
 /**
  * Action Creators
@@ -61,6 +64,28 @@ export const addCard = (card) => {
   }
 };
 
+export const addUser = (user) => {
+  return (dispatch) => {
+    return fetch('/auth/register', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        console.log('adduser response', response)
+        return response.json();
+      })
+      .then((body) => {
+        return dispatch({
+          type: ADD_USER,
+          payload: body
+        })
+      });
+  }
+};
+
 export const deleteCard = (id) => {
   return (dispatch) => {
     return fetch('/cards', {
@@ -98,6 +123,42 @@ export const editCard = (card) => {
         return dispatch({
           type: EDIT_CARD,
           payload: cards
+        });
+      });
+  }
+};
+
+export const loginUser = (user) => {
+  return (dispatch) => {
+    return fetch('/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((success) => {
+        return dispatch({
+          type: LOGIN_USER,
+          payload: success
+        });
+      });
+  }
+};
+
+export const logoutUser = () => {
+  return (dispatch) => {
+    return fetch('/auth/logout')
+      .then((response) => {
+        return response.json();
+      })
+      .then((success) => {
+        return dispatch({
+          type: LOGOUT_USER,
+          payload: success
         });
       });
   }
