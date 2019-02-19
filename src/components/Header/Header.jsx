e69ff58c58b55f056cefd30eef919cc724788a24
Brad McKinney <brad.mckin@gmail.com>
2019-02-19 11:27:49 -1000

    adjust header render function and styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './Header.scss';
 import AddCardButton from '../../containers/AddCardButton';
+import LoginButton from '../../containers/LoginButton';
+import LogoutButton from '../../containers/LogoutButton';
 
 const Header = (props) => {
-  const { header } = props;
+  const { header, auth } = props;
+
+  if (auth) {
+    return (
+      <div className="header">
+        <div className="kanban-tools">
+          <h1 className="header-title">{header}</h1>
+          <AddCardButton />
+        </div>
+        <LogoutButton />
+      </div>
+    )
+  }
 
   return (
     <div className="header">
-      <h1 className="header-title">{header}</h1>
-      <AddCardButton />
+      <div className="kanban-tools">
+        <h1 className="header-title">{header}</h1>
+      </div>
+      <LoginButton />
     </div>
   )
 };
