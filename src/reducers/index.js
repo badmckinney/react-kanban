6770aed30a631626205cc64dc098af621a08a35f
Brad McKinney <brad.mckin@gmail.com>
2019-02-19 11:30:50 -1000

    add an auth property to state and add ADD_USER, LOGIN_USER, and LOGOUT_USER cases to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
-import { LOAD_CARDS, LOAD_USERS, ADD_CARD, EDIT_CARD, DELETE_CARD } from '../actions';
+import { LOAD_CARDS, LOAD_USERS, ADD_CARD, ADD_USER, EDIT_CARD, DELETE_CARD, LOGIN_USER, LOGOUT_USER } from '../actions';
 
 const cardReducer = (state = {
   cards: [],
-  users: []
+  users: [],
+  auth: localStorage.getItem('session')
 }, action) => {
   switch (action.type) {
     case LOAD_CARDS:
@@ -11,10 +12,21 @@ const cardReducer = (state = {
       return Object.assign({}, state, { users: action.payload });
     case ADD_CARD:
       return Object.assign({}, state, { cards: [...state.cards, action.payload] });
+    case ADD_USER:
+      return Object.assign({}, state, { users: [...state.users, action.payload] });
     case EDIT_CARD:
       return Object.assign({}, state, { cards: action.payload });
     case DELETE_CARD:
       return Object.assign({}, state, { cards: action.payload });
+    case LOGIN_USER:
+      if (action.payload.success) {
+        localStorage.setItem('session', true);
+        return Object.assign({}, state, { auth: localStorage.getItem('session') });
+      }
+      return Object.assign({}, state, { auth: localStorage.getItem('session') });
+    case LOGOUT_USER:
+      localStorage.removeItem('session');
+      return Object.assign({}, state, { auth: localStorage.getItem('session') });
     default:
       return state;
   }
