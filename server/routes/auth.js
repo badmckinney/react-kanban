173d47d8d2594098d81dc6a6bb8b91446a757e2e
Brad McKinney <brad.mckin@gmail.com>
2019-02-19 11:28:59 -1000

    create a router for login/authentication routes

---
+++ server/routes/auth.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const User = require('../../database/models/User');
+
+const saltRounds = 12;
+
+/************************
+ *  REGISTER
+************************/
+
+router.post('/register', (req, res) => {
+  User.where({ username: req.body.username }).fetch()
+    .then((dbUser) => {
+      if (dbUser) {
+        req.flash('error', 'That username already exists')
+        return res.json({ success: false });
+      }
+
+      bcrypt.genSalt(saltRounds, (err, salt) => {
+        if (err) {
+          res.status(500);
+          res.send(err);
+        }
+
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+          if (err) {
+            res.status(500);
+            res.send(err);
+          }
+
+          return new User({
+            username: req.body.username,
+            password: hash,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email
+          })
+            .save()
+            .then((user) => {
+              return res.json({ success: true });
+            })
+            .catch((err) => {
+              req.flash('error', 'Error creating account')
+              return res.json({ success: false });
+            });
+        });
+      });
+    });
+});
+
+/************************
+ *  LOGIN
+************************/
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  return res.json({ success: true });
+});
+
+/************************
+ *  LOGOUT
+************************/
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  return res.json({});
+});
+
+module.exports = router;
